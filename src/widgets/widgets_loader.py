fc847519922c97519e401621d8e046d0e938ef4c
Daniele Verducci <daniele.verducci@ichibi.eu>
2024-04-04 08:54:38 +0200

    Aperture priority page

--- src/widgets/widgets_loader.py
+++ src/widgets/widgets_loader.py
@@ -1,8 +1,10 @@
 from gi.repository import GObject
 
 from .time_priority_page import TimePriorityPage
+from .aperture_priority_page import AperturePriorityPage
 
 # Register widgets to be used in templates UI
 def registerWidgets() -> None:
     GObject.type_ensure(TimePriorityPage)
+    GObject.type_ensure(AperturePriorityPage)
 
