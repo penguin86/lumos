b71c74e7f17c1ecce0722b3486528ff14793ee6c
Daniele Verducci <daniele.verducci@ichibi.eu>
2024-04-10 08:07:39 +0200

    Manual exposure page

--- src/widgets/widgets_loader.py
+++ src/widgets/widgets_loader.py
@@ -2,9 +2,11 @@ from gi.repository import GObject
 
 from .time_priority_page import TimePriorityPage
 from .aperture_priority_page import AperturePriorityPage
+from .manual_exposure_page  import ManualExposurePage
 
 # Register widgets to be used in templates UI
 def registerWidgets() -> None:
     GObject.type_ensure(TimePriorityPage)
     GObject.type_ensure(AperturePriorityPage)
+    GObject.type_ensure(ManualExposurePage)
 
